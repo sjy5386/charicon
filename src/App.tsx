3075b03ced26b60fee810267bcde9acd5841bfe5
Seokjin Yoon <13289043+sjy5386@users.noreply.github.com>
2024-12-24 20:46:47 +0900

    위치 조절 추가
    Signed-off-by: Seokjin Yoon <13289043+sjy5386@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,14 @@ import './App.css'
 function App() {
     const canvasRef = useRef(null)
 
+    const size = 100
+
     const [character, setCharacter] = useState('글')
     const [backgroundColor, setBackgroundColor] = useState('black')
     const [color, setColor] = useState('white')
+
+    const [x, setX] = useState(8)
+    const [y, setY] = useState(80)
 
     useEffect(() => {
         const canvas = canvasRef.current;
@@ -19,24 +24,34 @@ function App() {
         ctx.fillRect(0, 0, 100, 100);
 
         // 글자 쓰기
-        ctx.font = "90px ChosunGs";
+        ctx.font = `90px ChosunGs`;
         ctx.fillStyle = color;
-        ctx.fillText(character, 8, 80);
+        ctx.fillText(character, x, y);
     })
 
     return (
         <>
             <div>
-                <canvas ref={canvasRef} width="100" height="100"></canvas>
+                <canvas ref={canvasRef} width={size} height={size}></canvas>
             </div>
             <h1>글자티콘 생성기</h1>
             <div className="card">
-                <label>글자</label>
-                <input type="text" maxLength={1} value={character} onChange={(e) => setCharacter(e.target.value)}/>
-                <label>배경색</label>
-                <input type="color" value={backgroundColor} onChange={(e) => setBackgroundColor(e.target.value)}/>
-                <label>글자색</label>
-                <input type="color" value={color} onChange={(e) => setColor(e.target.value)}/>
+                <div>
+                    <label>글자</label>
+                    <input type="text" maxLength={1} value={character} onChange={(e) => setCharacter(e.target.value)}/>
+                    <label>배경색</label>
+                    <input type="color" value={backgroundColor} onChange={(e) => setBackgroundColor(e.target.value)}/>
+                    <label>글자색</label>
+                    <input type="color" value={color} onChange={(e) => setColor(e.target.value)}/>
+                </div>
+                <div>
+                    <label>X</label>
+                    <input type="range" value={x} onChange={(e) => setX(Number(e.target.value))}
+                           max={size} min={-size} id="x-range-input"/>
+                    <label>Y</label>
+                    <input type="range" value={y} onChange={(e) => setY(Number(e.target.value))}
+                           max={size * 2} min={-size / 2} id="y-range-input"/>
+                </div>
             </div>
         </>
     )
